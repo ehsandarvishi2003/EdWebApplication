755d593d90216a232691613a5376962b751e7288
Fateme HKazemi <fhkazemi4991@gmail.com>
2023-12-18 16:15:07 +0330

    Add project files.

---
+++ EdWebApplication/Models/Owner.cs
@@ -0,0 +1,11 @@
+﻿namespace EdWebApplication.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+        public Country country { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+    }
+}
---
+++ EdWebApplication/Models/PokemonOwner.cs
@@ -0,0 +1,10 @@
+﻿namespace EdWebApplication.Models
+{
+    public class PokemonOwner
+    {
+        public int PokemonId { get; set; }
+        public int OwnerId { get; set; }
+        public Pokemon Pokemon { get; set; } //-->join tables
+        public Owner Owner { get; set; }
+    }
+}
