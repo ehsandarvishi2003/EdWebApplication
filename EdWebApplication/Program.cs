11b4ff46599f0e10fda7813f1d81ebc142903b74
Fateme HKazemi <fhkazemi4991@gmail.com>
2023-12-19 15:14:34 +0330

    add connection string in Program File setup

--- EdWebApplication/Program.cs
+++ EdWebApplication/Program.cs
@@ -1,4 +1,7 @@
 
+using EdWebApplication.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace EdWebApplication
 {
     public class Program
@@ -13,6 +16,10 @@ namespace EdWebApplication
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<DataContext>(options=>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
